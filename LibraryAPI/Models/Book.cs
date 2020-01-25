933283ae0c434f53774dacbbfbdf11409eb03365
Norbert <norbert.scehura@gmail.com>
2020-01-25 19:39:54 +0100

    add more props to book model

--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -23,9 +23,13 @@ namespace LibraryAPI.Models
         [Required]
         public string Author { get; set; }
 
-        public string Category { get; set; }
+        public List<string> Category { get; set; }
 
         public int Pages { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Type { get; set; }
 
         public DateTime PublicationDate { get; set; }
     }
